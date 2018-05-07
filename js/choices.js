6444d07cb03e1a405c0ec8bb173dfbd1995010fc
Scott Enderle <scott.enderle@gmail.com>
2018-05-06 20:32:21 -0400

    better oo design and output

--- js/choices.js
+++ js/choices.js
@@ -1,39 +1,61 @@
 document.modules = document.modules || {};
 document.modules.util = document.modules.util || {};
 (function(ns) {
-    ns.Chooser = async function(url) {
-        let chooser = {
-            choose: function(attr) {
-                let n = this.choices.length;
-                let i = Math.floor(Math.random() * n);
-                return this.choices[i][attr];
-            },
-            getChoices: function() {
-                let result = {};
-                for (let i = 0; i < this.choice_attrs.length; i++) {
-                    let attr = this.choice_attrs[i];
-                    result[attr] = this.choose(attr);
+    ns.Chooser = function(url, output) {
+        this.choose = function(attr) {
+           let n = this.choices.length;
+           let i = Math.floor(Math.random() * n);
+           return this.choices[i][attr];
+        };
+        this.getChoice = function() {
+            let result = {};
+            for (let i = 0; i < this.choice_attrs.length; i++) {
+                let attr = this.choice_attrs[i];
+                result[attr] = this.choose(attr);
+            }
+            return result;
+        };
+        this.renderChoice = function() {
+            let result = this.getChoice();
+            for (let i = 0; i < this.choice_attrs.length; i++) {
+                let attr = this.choice_attrs[i];
+                let r = this.renderers[attr];
+                if (r) {
+                    r(attr, result[attr]); 
                 }
-                return result;
-            },
-            formatChoices: function() {
-                let result = this.getChoices();
-                let toformat = [];
-                for (let i = 0; i < this.choice_attrs.length; i++) {
-                    let attr = this.choice_attrs[i];
-                    toformat.push(attr);
-                    toformat.push(': ');
-                    toformat.push(this.choose(attr));
-                    toformat.push(', ');
-                }
-                toformat.pop();
-                return toformat.join('');
+            }
+        };
+        this.addRenderer = function(attr) {
+            let attrDiv = document.createElement('div');
+            attrDiv.classList.add('chooser-attr-output-div');
+            let attrTn = document.createTextNode('');
+            attrDiv.appendChild(attrTn);
+            let valueDiv = document.createElement('div');
+            valueDiv.classList.add('chooser-value-output-div');
+            let valueTn = document.createTextNode('');
+            valueDiv.appendChild(valueTn);
+            this.output.appendChild(attrDiv);
+            this.output.appendChild(valueDiv);
+            
+            this.renderers[attr] = function(attr, value) {
+                attrTn.nodeValue = attr + ':';
+                valueTn.nodeValue = value;
+            }
+        };
+        this.setChoices = function(choices) {
+            this.choices = choices;
+            this.choice_attrs = Object.keys(choices[0]);
+            for (let i = 0; i < this.choice_attrs.length; i++) {
+                this.addRenderer(this.choice_attrs[i]);
             }
         };
 
-        let choices = await d3.csv(url)
-        chooser.choices = choices;
-        chooser.choice_attrs = Object.keys(choices[0]);
-        return chooser;
+        this.output = output;
+        this.renderers = {};
+        this.setChoices([{}]);
+        this.promise = d3.csv(url).then(choices => {
+            this.setChoices(choices);
+            return this;
+        });
     }
 })(document.modules.util);
